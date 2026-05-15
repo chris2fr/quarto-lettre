eeef2d17e56329514aa1c98f3739e0041d51fcbc
Chris Mann <chris@lesgtrandsvoisins.com>
2026-05-15 09:05:25 +0200

    Handles definition lists as tables in pdf

--- _extensions/lettre/pdf/_filters/divs.lua
+++ _extensions/lettre/pdf/_filters/divs.lua
@@ -16,6 +16,25 @@ local function wrap_div(el, env)
   return el
 end
 
+local function to_latex(blocks)
+  return pandoc.write(pandoc.Pandoc(blocks), 'latex'):gsub('\n$', '')
+end
+
+function DefinitionList(el)
+  local rows = {}
+  for _, item in ipairs(el.content) do
+    local term = to_latex({ pandoc.Plain(item[1]) })
+    local def  = item[2] and item[2][1] and to_latex({ item[2][1][1] }) or ''
+    table.insert(rows, term .. ' & ' .. def .. ' \\\\')
+  end
+  return pandoc.RawBlock('latex',
+    '\\begin{longtable}{@{}p{0.3\\linewidth}p{0.7\\linewidth}@{}}\n' ..
+    '\\toprule\n' ..
+    table.concat(rows, '\n') .. '\n' ..
+    '\\bottomrule\n' ..
+    '\\end{longtable}')
+end
+
 function Div(el)
   for class, env in pairs(env_for_class) do
     if el.classes:includes(class) then
