d725373f486a66a815d08bb0264dcd15d940b882
Chris Mann <chris@lesgtrandsvoisins.com>
2026-05-22 15:18:02 +0200

    Something about the warnings and french=guillemets

--- _extensions/compte-rendu/_filters/validate.lua
+++ _extensions/compte-rendu/_filters/validate.lua
@@ -1,5 +1,4 @@
 local required_meta = { 'title', 'author', 'lang', 'date' }
-local optional_meta = { 'place', 'organization', 'ref' }
 local required_divs = { 'participants', 'body' }
 
 local seen = {}
@@ -13,10 +12,6 @@ function Div(el)
 end
 
 function Pandoc(doc)
-  local known_meta = {}
-  for _, k in ipairs(required_meta) do known_meta[k] = true end
-  for _, k in ipairs(optional_meta) do known_meta[k] = true end
-
   local blocks = {}
   for _, block in ipairs(doc.blocks) do
     if block.t == 'Div' then
@@ -33,12 +28,6 @@ function Pandoc(doc)
   end
   if #missing > 0 then
     error('Compte-rendu: missing required metadata: ' .. table.concat(missing, ', '))
-  end
-
-  for key, _ in pairs(doc.meta) do
-    if key:match('^%l[%l%-]*$') and not known_meta[key] then
-      io.stderr:write('WARNING [compte-rendu]: unrecognized metadata key: ' .. key .. '\n')
-    end
   end
 
   for _, class in ipairs(required_divs) do
